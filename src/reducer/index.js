60bafbed5eb71a871aafdf293c106edd92ade463
NADAVLAP\נדב פופוביץ <nitzanp44@gmail.com>
2022-02-21 14:03:29 +0200

    #2

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import sideBarReducer from "./sideBarReducer";
 import isLoginReducer from "./isLoginReducer";
+import userReducer from "./userReducer"
 
 const reducers = combineReducers({
     sideBar: sideBarReducer,
-    isLogin: isLoginReducer
+    isLogin: isLoginReducer,
+    user: userReducer
 });
 
 export default reducers;
