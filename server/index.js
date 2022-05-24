1501a1d5e3022782f8e2ef09bc4e3d2a97899232
Reut-Levenberg <85216945+Reut-Levenberg@users.noreply.github.com>
2022-05-24 12:07:47 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ let mongoose = require('mongoose');
 let indexRoutes =require ('./routes/index.js');
 var cors = require('cors')
 let bodyParser=require('body-parser')
-let config=require('./config');
+require("dotenv").config({ path: "./config.env" });
+const Db = process.env.MongoURL;
 
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use('/', indexRoutes);
 
-const CONNECTION_URL = config.MongoURL;
+const CONNECTION_URL = Db;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
